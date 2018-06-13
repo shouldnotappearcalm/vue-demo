e7edf68da0f0e22022d57fe6ef31645a48b02c2f
gaozhirong <gaozhirong@thunisoft.com>
2018-06-13 18:32:38 +0800

    新增iview控件使用

--- iview-demo01/src/router/index.js
+++ iview-demo01/src/router/index.js
@@ -3,12 +3,16 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Grid from '../view/grid/grid'
 import Layout from '../view/layout/index'
+import Select from '../view/select/select'
+import Date from '../view/date/date'
+import Transfer from '../view/transfer/transfer'
+import Form from '../view/form/form'
+import Tree from '../view/tree/tree'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
@@ -22,6 +26,31 @@ export default new Router({
       path: '/layout',
       name: 'Layout',
       component: Layout
+    },
+    {
+      path: '/select',
+      name: 'Select',
+      component: Select
+    },
+    {
+      path: '/date',
+      name: 'Date',
+      component: Date
+    } ,
+    {
+      path: '/transfer',
+      name: 'Transfer',
+      component: Transfer
+    },
+    {
+      path: '/form',
+      name: 'Form',
+      component: Form
+    },
+    {
+      path: '/tree',
+      name: 'Tree',
+      component: Tree
     }
   ]
 })
---
+++ iview-demo01/src/view/date/date.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <DatePicker type="date" placeholder="Select date" style="width:200px" @on-change="changeDate" ></DatePicker>
+        <DatePicker type="daterange" v-model="date2" placeholder="Select date" style="width:200px" ></DatePicker>
+        <span>{{date1}} ---  {{date2}}</span>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            date1:'',
+            date2:''
+        };
+    },
+    methods:{
+        changeDate(dateTime) {
+            console.log(dateTime)
+            this.date1=dateTime
+        }
+    }
+}
+</script>
+
